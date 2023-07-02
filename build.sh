e2921666a820fff683b9e743c3f3a1d8d0aaf7cd
yxx <1026892284@qq.com>
2023-07-03 01:44:18 +0800

    move py_modules to defaults

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
  #!/bin/bash
 pnpm i
 pnpm run build
+cp -r defaults/py_modules py_modules
 tar -czvf MangoPeel.tar.gz --transform 's,^,MangoPeel/,' assets dist py_modules decky_plugin.pyi package.json plugin.json main.py README.md README_CN.md LICENSE
+rm -rf py_modules
