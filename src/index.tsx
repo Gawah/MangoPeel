aae31d6538e87c6e8f6495968a8c5d88a34b4019
Gawah <1026892284@qq.com>
2023-12-06 00:17:55 +0800

    修复可能刷出空分页的问题

--- src/index.tsx
+++ src/index.tsx
@@ -16,42 +16,49 @@ import ParamGroupTabs from "./components/ParamGroupTabs";
 
 
 const Content: VFC<{ serverAPI: ServerAPI }> = ({}) => {
+  const getNewProps=()=>{
+    return Object.values(ParamGroup).map((groupName) => {
+      var groupItem = Object.values(Config.paramList).filter((paramData) => {
+        return paramData.group == groupName;
+      });
+      return { groupName, groupItem };
+    }).filter(({ groupName, groupItem }) => {
+      if (groupItem.length == 0)
+        return false;
+      return Settings.getGroupVisible(Settings.getSettingsIndex(), groupName);
+    }).map(({ groupName, groupItem }) => {
+      return {
+        label: groupName, id: groupName, Node: <div>
+          {groupItem.map((paramData) => {
+            return (
+              <div>
+                <ParamItem key={paramData.name} paramData={paramData}></ParamItem>
+              </div>);
+          })}
+        </div>
+      };
+    })
+  }
+  const [props,setprops]=useState<{label:string,id:string,Node:any}[]>(getNewProps());
+  useEffect(() => {
+    const updateProps=()=>{
+      setprops(getNewProps())
+    }
+    Object.values(ParamGroup).map((groupName) => {
+      Settings.settingChangeEventBus.addEventListener(groupName, updateProps);
+    })
+    return () => {
+      Object.values(ParamGroup).map((groupName) => {
+        Settings.settingChangeEventBus.addEventListener(groupName, updateProps);
+      })
+    };
+  }, []);
   return (
     <>
       <PanelSection>
         <MangoIndex></MangoIndex>
       <>
-        <ParamGroupTabs props={Object.values(ParamGroup).map((groupName) => {
-            var groupItem = Object.values(Config.paramList).filter((paramData) => {
-              return paramData.group == groupName;
-            });
-            return { groupName, groupItem };
-          }).filter(({ groupName, groupItem }) => {
-            if (groupItem.length == 0)
-              return false;
-            const [visible, setVisible] = useState(Settings.getGroupVisible(Settings.getSettingsIndex(), groupName));
-            useEffect(() => {
-              const updateEvent = () => {
-                setVisible(Settings.getGroupVisible(Settings.getSettingsIndex(), groupName));
-              };
-              Settings.settingChangeEventBus.addEventListener(groupName, updateEvent);
-              return () => {
-                Settings.settingChangeEventBus.removeEventListener(groupName, updateEvent);
-              };
-            }, []);
-            return visible;
-          }).map(({ groupName, groupItem }) => {
-            return {
-              label: groupName, id: groupName, Node: <div>
-                {groupItem.map((paramData) => {
-                  return (
-                    <div>
-                      <ParamItem key={paramData.name} paramData={paramData}></ParamItem>
-                    </div>);
-                })}
-              </div>
-            };
-          })}></ParamGroupTabs>
+        <ParamGroupTabs props={props}></ParamGroupTabs>
       </>
       </PanelSection>
       <PanelSection>
